6395a737ce7e1bb9fe4dbc280d0b32b77114af0e
philomathtanya <tanyabansal525@gmail.com>
2023-05-18 22:06:26 +0530

    oops-1

---
+++ src/oops1/Person_client.java
@@ -0,0 +1,15 @@
+package oops1;
+
+public class Person_client {
+    public static void main(String[] args) {
+        Person obj=new Person("ritesh verma",21);// Using parameterised consturctor
+//        obj.intro();
+//        obj.name="ritesh"; // Data Member is not accessed in private type
+//        obj.age=25;
+//        obj.setAge(25);
+//        obj.setName("Ritesh Verma");
+//        System.out.println(obj.getAge());
+        obj.setAge(-2);
+        obj.intro();
+    }
+}
