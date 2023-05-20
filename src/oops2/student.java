d26310556b8ec62f348e958fcc5c9c553717b468
philomathtanya <tanyabansal525@gmail.com>
2023-05-20 15:38:59 +0530

    oops and inheritance

---
+++ src/oops2/student.java
@@ -0,0 +1,14 @@
+package oops2;
+
+public class student {
+
+    String name;
+    int age;
+
+    public void Intro_yourSelf() {
+        String name = "Kaju";
+        System.out.println("My Name is " + this.name + " and age is " + this.age);
+
+    }
+
+}
